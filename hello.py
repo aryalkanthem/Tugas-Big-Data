9ece1283c32f19470b3894d68b739056e056366c
aryalkanthem <82429373+aryalkanthem@users.noreply.github.com>
2021-04-14 21:58:30 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("===============================")
+print("Biodata Saya")
+print("===============================")
+
+print("Nama Lengkap : Ari Budi Utomo")
+print("Kelas : Semester 6")
+print("Nama Kampus: Universitas Narotama Surabaya ")
