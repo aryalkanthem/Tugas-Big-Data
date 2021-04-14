691c20fa493f0df9b786db17b343890a65d5a6fe
aryalkanthem <82429373+aryalkanthem@users.noreply.github.com>
2021-04-14 22:00:07 +0700

    Delete hello.py

--- hello.py
+++
@@ -1,7 +0,0 @@
-print("===============================")
-print("Biodata Saya")
-print("===============================")
-
-print("Nama Lengkap : Ari Budi Utomo")
-print("Kelas : Semester 6")
-print("Nama Kampus: Universitas Narotama Surabaya ")
